5815477f4773ff76c17df1d046f8c100f88ccf9b
Miguel T Rivera <10368105+mtrivera@users.noreply.github.com>
2025-11-08 23:26:55 -0800

    feat: Pass error test negative numbers 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,16 +1,22 @@
 const sumAll = function(startNum, endNum) {
 
-  if (startNum > endNum) {
-    tempEndNum = startNum;
-    startNum = endNum;
-    endNum = tempEndNum;
-  }
+  const isPositiveNums = [startNum, endNum].every(num => num > 0);
 
-  let sum = 0;
-  for (let count = startNum; count <= endNum; count++) {
-    sum += count;
+  if (isPositiveNums) {
+    if (startNum > endNum) {
+      tempEndNum = startNum;
+      startNum = endNum;
+      endNum = tempEndNum;
+    }
+
+    let sum = 0;
+    for (let count = startNum; count <= endNum; count++) {
+      sum += count;
+    }
+    return sum;
+  } else {
+    return 'ERROR';
   }
-  return sum;
 };
 
 // Do not edit below this line
