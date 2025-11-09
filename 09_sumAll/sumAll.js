c57f56cda7d4b5497a27a5c8b299f06b3b6020eb
Miguel T Rivera <10368105+mtrivera@users.noreply.github.com>
2025-11-08 23:14:25 -0800

    feat: Pass first larger number test 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+  if (startNum > endNum) {
+    tempEndNum = startNum;
+    startNum = endNum;
+    endNum = tempEndNum;
+  }
+
+  let sum = 0;
+  for (let count = startNum; count <= endNum; count++) {
+    sum += count;
+  }
+  return sum;
 };
 
 // Do not edit below this line
