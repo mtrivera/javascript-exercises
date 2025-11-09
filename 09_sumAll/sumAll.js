ec19123a53593d9688c2ddaaf5d12cd9d20ebfd0
Miguel T Rivera <10368105+mtrivera@users.noreply.github.com>
2025-11-09 00:19:54 -0800

    refactor: Convert three bool check into one 09-sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -3,8 +3,9 @@ const sumAll = function(startNum, endNum) {
   const isPositiveNums = [startNum, endNum].every(num => num > 0);
   const isIntegerNums = [startNum, endNum].every(num => Number.isInteger(num));
   const isTypeNums = [startNum, endNum].every(num => typeof num === 'number');
+  const isValidNums = isPositiveNums && isIntegerNums && isTypeNums;
 
-  if (isPositiveNums && isIntegerNums && isTypeNums) {
+  if (isValidNums) {
     if (startNum > endNum) {
       tempEndNum = startNum;
       startNum = endNum;
